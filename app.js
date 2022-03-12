7e8ecc4d5c783fce358b3ead55c6672b660a4105
Eren Saraç <esarac@outlook.com.tr>
2022-03-13 00:38:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,43 +2,37 @@ const body = document.querySelector("body")
 const todoContainer = document.querySelector("#todo-container")
 const button = document.querySelector("#create-todo-btn")
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; // Months Array
+
 document.addEventListener("DOMContentLoaded", (todo) => {
-    console.log(`todo is loaded ${todo}`);
     let todos
-    if (localStorage.getItem("todos") == ""){
+    if (localStorage.getItem("todos") === null){
         todos = []
     } else {
         todos = JSON.parse(localStorage.getItem("todos"))
-    }
-    for (let i = 0; i < todos.length; i++){
-        createTodo(todos[i].name, todos[i].createdDate)
-    }
-    check()
+        for (let i = 0; i < todos.length; i++){
+            createTodo(todos[i].name, todos[i].createdDate, todos[i].isCompleted)
+        }
+    } checkTodo()
 })
-function check(){
-    let checkBtn = document.querySelectorAll(".checkbox")
-    checkBtn.forEach(function(e){ 
-        let isClicked = false
-        e.addEventListener("click", ()=>{
-            console.log("Check Todo Button is working.")
-            let checkBtn = document.querySelectorAll(".checkbox")
-            if (isClicked === false){
-                e.classList.remove("ri-checkbox-blank-line")
-                e.classList.add("ri-checkbox-fill")
-                let parent = e.parentElement
-                parent.parentElement.classList.add("completed-todo")
-                isClicked = true
-            } else {
-                e.classList.remove("ri-checkbox-fill")
-                e.classList.add("ri-checkbox-blank-line")
-                let parent = e.parentElement
-                parent.parentElement.classList.remove("completed-todo")
-                isClicked = false
-            }
-        })
-    })
-}
-function createTodo (inputValue, time){
+
+button.addEventListener("click", ()=> { 
+    const todoInput = document.querySelector("#todo-input").value
+    let time = [new Date().getHours(),new Date().getMinutes(),new Date().getDate(),months[new Date().getMonth()],new Date().getFullYear()]
+    if (time[1] < 10) {
+        time[1] = "0" + time[1]
+    }
+    let fullTime = `${time[0]}:${time[1]} • ${time[2]} ${time[3]} ${time[4]}`
+    if(todoInput == ""){
+        alertMsg("warn", "Please do not leave the input blank, fill the input.")
+    } else {
+        createTodo(todoInput, fullTime)
+        saveLS(todoInput, fullTime)
+        checkTodo()
+        alertMsg("success", "Todo has been successfully created.")
+    }
+})
+
+function createTodo(inputValue, time, isCompleted){
     const todoInput = document.querySelector("#todo-input")
     let listItem = document.createElement("LI")
     let todoTextContent = document.createElement("DIV")
@@ -59,74 +53,64 @@ function createTodo (inputValue, time){
     pElement.textContent = inputValue
     todoInput.value = ""
 }
-button.addEventListener("click", ()=> { 
-    const todoInput = document.querySelector("#todo-input").value
-    let time = [
-        new Date().getHours(),
-        new Date().getMinutes(),
-        new Date().getDate(),
-        months[new Date().getMonth()],
-        new Date().getFullYear()
-    ]
-    if (time[1] < 10){ 
-        time[1] = "0" + time[1]
-    }
-    let fullTime = `${time[0]}:${time[1]} • ${time[2]} ${time[3]} ${time[4]}`
-    if(todoInput == ""){
-        alertMsg("warn", "Please do not leave the input blank, fill the input.")
-    } else {
-        createTodo(todoInput, fullTime)
-        saveLS(todoInput, fullTime)
 
-        const deleteBtn = document.querySelectorAll(".delete")  // Delete Todo Button
-        deleteBtn.forEach( (e) => {
-            e.addEventListener("click", ()=>{
-                console.log("Delete Todo Button is working.")
-            })
+function checkTodo(){ //
+    let checkBtn = document.querySelectorAll(".checkbox")
+    checkBtn.forEach(function(e){
+        let isCompleted = false
+        e.addEventListener("click", ()=>{
+            console.log("Check Todo Button is working.")
+            if (isCompleted == false){
+                e.classList.remove("ri-checkbox-blank-line")
+                e.classList.add("ri-checkbox-fill")
+                let parent = e.parentElement
+                parent.parentElement.classList.add("completed-todo")
+                isCompleted = true
+            } else {
+                e.classList.remove("ri-checkbox-fill")
+                e.classList.add("ri-checkbox-blank-line")
+                let parent = e.parentElement
+                parent.parentElement.classList.remove("completed-todo")
+                isCompleted = false
+            }
         })
-        check()
-        alertMsg("success", "Todo has been successfully created.")
-    }
-})
+    })
+}
+
 function saveLS(todo, time) {
     let todos
-    if (localStorage.getItem("todos") === null){
-        todos = []
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"))
-    }
-    todos.push(
-            {
-                "name": todo,
-                "createdDate": time,
-                "isCompleted": false
-            }
-        )
+    if (localStorage.getItem("todos") === null){todos = []} 
+    else {todos = JSON.parse(localStorage.getItem("todos"))}
+    todos.push({
+        "name": todo,
+        "createdDate": time,
+        "isCompleted": false
+    })
     localStorage.setItem("todos", JSON.stringify(todos))
 }
-function alertMsg(alertType, alertMessage){
-    if (document.querySelector("#notification") !== null){
-        document.querySelector("#notification").remove()
+
+function deleteAllTodo(){
+    let allTodoItems = document.querySelectorAll(".todolist-item")
+    for (let i = 0; i < allTodoItems.length; i++){
+        allTodoItems[i].remove()
     }
-    let alertElement = document.createElement("DIV")
-    alertElement.id = "notification"
-    body.append(alertElement)
-    if (alertType == "success"){
-        alertElement.classList.add("alert", "success")
-        alertElement.innerText = alertMessage
-    } else if (alertType == "danger"){
-        alertElement.classList.add("alert", "danger")
-        alertElement.innerText = alertMessage
-    } else {
-        alertElement.classList.add("alert", "warn")
-        alertElement.innerText = alertMessage
+    localStorage.removeItem("todos")
+    alertMsg("danger", "All todos has been clear.")
+}
+
+function alertMsg(type, msg){
+    if (document.querySelector("#notification") !== null){ 
+          document.querySelector("#notification").remove()
     }
+    let e = document.createElement("DIV")
+    e.id = "notification"
+    body.append(e)
+    e.classList.add("alert", `${type}`)
+    e.innerText = msg 
+    e.style.opacity = "1"
     setTimeout(()=> {
-        alertElement.style.opacity = "1"
-    }, 10)
-    setTimeout(()=> {
-        alertElement.style.opacity = "0"
-        alertElement.style.display = "none"
-        alertElement.remove()
+        e.style.opacity = "0"
+        e.style.display = "none"
+        e.remove()
     }, 3500)
 }
