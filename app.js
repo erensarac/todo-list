a0d3c5e4252dbe218c46d356517932081d88ea47
Eren Saraç <esarac@outlook.com.tr>
2022-04-08 23:50:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const todoContainer = document.querySelector("#todo-container")
 const button = document.querySelector("#create-todo-btn")
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; // Months Array
 
+
 // Dom Content Loaded
-document.addEventListener("DOMContentLoaded", (todo) => {
+document.addEventListener("DOMContentLoaded", () => {
     let todos
     if (localStorage.getItem("todos") === null){
         todos = []
@@ -14,7 +15,9 @@ document.addEventListener("DOMContentLoaded", (todo) => {
         for (let i = 0; i < todos.length; i++){
             CREATE_TODO(todos[i].id, todos[i].name, todos[i].createdDate, todos[i].isCompleted)
         }
-    } CHECK_TODO() // Fonksiyonu bir kere calistirmali.
+        CHECK_TODO()
+    }
+    DELETE_TODO() // Fonksiyonu bir kere calistirmali.
 })
 
 // Create Todo Button Onclick 
@@ -36,13 +39,12 @@ button.addEventListener("click", ()=> {
         ALERT_MESSAGE("warn", "Please do not leave the input blank, fill the input.")
     } else {
         let id = `${time[3]}${new Date().getMonth()}${time[1]}${time[2]}${time[0]}${time[5]}`
-        console.log(id)
         CREATE_TODO(id, todoInput, fullTime, false)
         SAVE_LOCAL_STORAGE(id, todoInput, fullTime, false)
         ALERT_MESSAGE("success", "Todo has been successfully created.")
     }
 })
-
+let checkBtn
 // Create Todo Function
 function CREATE_TODO(todoID, inputValue, time, isCompleted){
     const todoInput = document.querySelector("#todo-input")
@@ -77,30 +79,38 @@ function CREATE_TODO(todoID, inputValue, time, isCompleted){
     checkBox.append(completeIcon)
     todoEl.append(text, createdDate)
     deleteBox.append(deleteBtn)
+    // Todo olusturulduktan sonra fonksiyonu tekrar cagirmak
+    DELETE_TODO()
 }
 
 // Todo Mark Completed 
 function CHECK_TODO(){
-    let checkBtn = document.querySelectorAll(".checkbox")
-    checkBtn.forEach(function(e){
+    checkBtn = document.querySelectorAll(".checkbox")
+    console.log("Fonksiyon calisiyor");
+    checkBtn.forEach((e)=>{
+        console.log(`e degeri = ${e}`);
         e.addEventListener("click", () => {
-            console.log("Check Todo Button is working.")
+            console.log("Click eventi calisiyor");
             todoID = e.parentElement.id
-            let todos
+            let todos, index
             if (localStorage.getItem("todos") === null){todos = []} 
             else {todos = JSON.parse(localStorage.getItem("todos"))}
-            index = todos.findIndex(x => x.id === todoID)
+            index = todos.findIndex(i => i.id === todoID)
+            console.log(`Kontrol edilen todo indeks = ${index}`);
             if (todos[index].isCompleted === false){
                 e.firstChild.classList.remove("ri-checkbox-blank-line")
                 e.firstChild.classList.add("ri-checkbox-fill")
                 e.parentElement.classList.add("completed")
                 todos[index].isCompleted = true
+                console.log("pozitif");
             } else {
+                console.log("negatif");
                 e.firstChild.classList.remove("ri-checkbox-fill")
                 e.firstChild.classList.add("ri-checkbox-blank-line")
                 e.parentElement.classList.remove("completed")
                 todos[index].isCompleted = false
             }
+            console.log("bosluk");
             localStorage.setItem("todos", JSON.stringify(todos))
         })
     })
@@ -120,8 +130,30 @@ function SAVE_LOCAL_STORAGE(id, msg, time, truefalse) {
     localStorage.setItem("todos", JSON.stringify(todos))
 }
 
+// Delete Todo Item
+function DELETE_TODO () {
+    let deleteBtn = document.querySelectorAll(".deletebox")
+    deleteBtn.forEach(e => {
+        e.addEventListener("click", ()=>{
+            console.log(e);
+            let todos, todoID
+            todoID = e.parentElement.id
+            if (localStorage.getItem("todos") === null){todos = []} 
+            else {todos = JSON.parse(localStorage.getItem("todos"))}
+            let index = todos.findIndex(todo => todo.id == `${todoID}`)
+            todos.splice(index, 1);
+            localStorage.setItem("todos",JSON.stringify(todos))
+            console.log(e.parentElement.id)
+            console.log(index)
+            e.parentElement.remove()
+            ALERT_MESSAGE("danger", "Todo has been deleted.")
+        })
+    })
+}
+
+
 // Delete All Todo Items
-function DELETE_ALL_TODO(){
+function DELETE_ALL_TODO () {
     let allTodoItems = document.querySelectorAll(".todo-item")
     for (let i = 0; i < allTodoItems.length; i++){
         allTodoItems[i].style.opacity = "0"
@@ -134,7 +166,7 @@ function DELETE_ALL_TODO(){
 }
 
 // Alert with type and message
-function ALERT_MESSAGE(type, msg){
+function ALERT_MESSAGE (type, msg) {
     if (document.querySelector("#notification") !== null){ 
           document.querySelector("#notification").remove()
     }
